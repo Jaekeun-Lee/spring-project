19e42a2fdbf18bfd165ead0f91f83c5399974d43
hy3in <hy3in01@gmail.com>
2021-01-14 16:28:38 +0900

    HYEIN : Update VO

---
+++ src/main/java/com/example/demo/projectApplicant/controller/ProjectApplicant.java
@@ -0,0 +1,5 @@
+package com.example.demo.projectApplicant.controller;
+
+public class ProjectApplicant {
+
+}
---
+++ src/main/java/com/example/demo/projectApplicant/service/impl/ProjectApplicantServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.demo.projectApplicant.service.impl;
+
+public class ProjectApplicantServiceImpl {
+
+}
